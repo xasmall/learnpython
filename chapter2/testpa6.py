c9ecd041b41cc0321d0fe54c1748e0146fd3e750
xasmall <xasmall@163.com>
2020-03-02 19:44:25 +0800

    chapter2 end

--- chapter2/testpa6.py
+++ chapter2/testpa6.py
@@ -21,3 +21,27 @@ print(list(map(lambda x,y: x+y,range(5),range(5,10))))
 x=random.randint(1,1e30)
 # 提取大整数每位上的数字
 print(list(map(int,str(x))))
+
+# reduce()可以将一个接受两个参数的函数以迭代累计的形式从左到右依次作用到一个序列或迭代器对象的所有元素
+
+from functools import reduce
+print(reduce(lambda x,y: x+y,range(1,10)))
+# filter()将一个单参数函数作用到一个序列上，返回该序列中使得该函数返回值为True的那些元素组成的filter对象，如果指定函数为None，则返回序列中等价于True的元素
+seq=['foo','x41','!?','***']
+def fun(x):
+    return x.isalnum()  #用于测试x是否为字母或数字
+
+print(list(filter(fun,seq)))
+
+# range()函数
+print(list(range(1,10,2))) #2是步长
+
+# zip()函数用于把多个可迭代对象中对应位置的元素压缩到一起，返回一个可迭代的zip对象
+print(list(zip('abcd','123')))
+print(list(zip('abcd')))
+print(list(zip('abcd','123',',.!')))
+
+# zip对象只能遍历一次，访问过的元素就不存在了
+x=zip('abcd','1234')
+print(list(x))
+print(list(x))
